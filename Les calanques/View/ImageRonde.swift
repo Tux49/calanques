c0694f44bfdc276adf8a8adcda802bb980379199
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-13 21:54:21 +0200

    Custom TableViewCell

---
+++ Les calanques/View/ImageRonde.swift
@@ -0,0 +1,30 @@
+//
+//  ImageRonde.swift
+//  Les calanques
+//
+//  Created by Thierry Huu on 13/10/2018.
+//  Copyright © 2018 Thierry Huu. All rights reserved.
+//
+
+import UIKit
+
+class ImageRonde: UIImageView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        layer.cornerRadius = frame.height / 2
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 2
+        clipsToBounds = true
+        contentMode = .scaleAspectFill
+    }
+}
